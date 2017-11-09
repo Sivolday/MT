bcf2dc6ff04f0442333bb2c4a5e28f0f14ff14f2
Sivolday <Sivolday@gmail.com>
2017-11-09 12:02:16 +0700

    Таймер

---
+++ MT.SharedComponents/ModelFEMElementDividerVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.SharedComponents
+{
+    public class ModelFEMElementDividerVM : ViewModelBase
+    {
+        private double _value = 2;
+        private bool _isMaxLenght;
+
+        public double Value
+        {
+            get { return _value; }
+            set { _value = value; RaisePropertyChanged();}
+        }
+
+        public bool IsMaxLenght
+        {
+            get { return _isMaxLenght; }
+            set { _isMaxLenght = value; RaisePropertyChanged();}
+        }
+    }
+}
---
+++ MT.SharedComponents/PerformanceTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace MT.SharedComponents
+{
+    public class PerformanceTester : ViewModelBase
+    {
+        public double Time
+        {
+            get => _time;
+            private set { _time = value; RaisePropertyChanged();}
+        }
+
+        private Stopwatch _watch;
+        private double _time;
+
+        public void Start()
+        {
+            _watch = Stopwatch.StartNew();
+        }
+
+        public void Stop()
+        {
+            _watch.Stop();
+            Time = _watch.ElapsedMilliseconds;
+        }
+    }
+}
